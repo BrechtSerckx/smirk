dc991336c6de0641738db7aa588ff28d1ea80b88
Brecht Serckx <services+git@brechtserckx.be>
2022-01-14 18:37:47 +0100

    Optparser

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -4,16 +4,39 @@ module Lib
 
 import           Control.Concurrent             ( threadDelay )
 import qualified Data.ByteString.Char8         as BS
+import           Options.Applicative
 import qualified System.Hardware.Serialport    as Serial
+
+pSerialPortSettings :: Parser Serial.SerialPortSettings
+pSerialPortSettings = do
+  timeout <-
+    option auto $ long "serial-timeout" <> value 10 <> showDefault <> help
+      "Serial timeout in 1/10th seconds"
+  pure Serial.defaultSerialSettings { Serial.timeout = timeout }
+
+data Opts = Opts
+  { serialPortSettings :: Serial.SerialPortSettings
+  , serialPortPath     :: FilePath
+  }
+
+pOpts :: Parser Opts
+pOpts = do
+  serialPortSettings <- pSerialPortSettings
+  serialPortPath     <-
+    strOption $ long "port" <> short 'p' <> value "/dev/ttyACM0" <> showDefault
+  pure Opts { .. }
+
+parseOpts :: IO Opts
+parseOpts =
+  let parserInfo = fullDesc <> progDesc "Smirk controller and server"
+  in  execParser $ (pOpts <**> helper) `info` parserInfo
 
 main :: IO ()
 main = do
+  Opts {..} <- parseOpts
   putStrLn "Hello World!"
 
-  let port = "/dev/ttyACM0"
-  serial <- Serial.openSerial
-    port
-    Serial.defaultSerialSettings { Serial.timeout = 10 }
+  serial <- Serial.openSerial serialPortPath serialPortSettings
 
   putStrLn "No-op"
   Serial.send serial "0"
