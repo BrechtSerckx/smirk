4cffe919fb3ec11204455408e01fc74f51415600
Brecht Serckx <services+git@brechtserckx.be>
2022-01-14 12:38:56 +0100

    Use picocom as monitor

--- shell.nix
+++ shell.nix
@@ -6,10 +6,12 @@ pkgs.mkShell {
     nixfmt
     arduino-mk
     arduino-core-unwrapped
+    picocom
   ];
   shellHook = with pkgs; ''
     cat > Base.mk << EOF
     ARDUINO_DIR = ${arduino-core-unwrapped}/share/arduino
+    MONITOR_CMD = picocom
     include ${arduino-mk}/Arduino.mk
     EOF
   '';
