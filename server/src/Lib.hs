a37ca9791c3dbb2493d87a2a47aa1fa996a95a8c
Brecht Serckx <services+git@brechtserckx.be>
2022-01-16 15:57:04 +0100

    Use `capability` for more abstractions

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -5,6 +5,8 @@ module Lib
 import           Control.Concurrent.Lock        ( Lock )
 import qualified Control.Concurrent.Lock       as Lock
 import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Reader     ( ReaderT(..) )
 import           Data.Acquire                   ( mkAcquire
                                                 , withAcquire
                                                 )
@@ -72,18 +74,30 @@ parseOpts =
   in  execParser $ (pOpts <**> helper) `info` parserInfo
 
 withSerialPort
-  :: Serial.SerialPort -> Lock -> (Serial.SerialPort -> IO a) -> IO a
-withSerialPort s lock f = Lock.with lock $ f s
+  :: MonadIO m
+  => Serial.SerialPort
+  -> Lock
+  -> (Serial.SerialPort -> IO a)
+  -> m a
+withSerialPort s lock f = liftIO . Lock.with lock $ f s
 
-serialSend :: Serial.SerialPort -> Lock -> BS.ByteString -> IO ()
+serialSend :: MonadIO m => Serial.SerialPort -> Lock -> BS.ByteString -> m ()
 serialSend s' l bs = void . withSerialPort s' l $ \s -> Serial.send s bs
 
 serialSendRecv
-  :: Serial.SerialPort -> Lock -> BS.ByteString -> IO BS.ByteString
+  :: MonadIO m => Serial.SerialPort -> Lock -> BS.ByteString -> m BS.ByteString
 serialSendRecv s' l bs = withSerialPort s' l $ \s -> do
   void $ Serial.send s bs
   Serial.recv s 10
 
+data Ctx = Ctx
+  { serialPort     :: Serial.SerialPort
+  , serialPortLock :: Lock
+  }
+  deriving stock Generic
+
+newtype M a = M { runM :: Ctx -> IO a }
+  deriving (Functor, Applicative, Monad, MonadIO) via ReaderT Ctx IO
 main :: IO ()
 main = do
   Opts {..} <- parseOpts
@@ -92,28 +106,30 @@ main = do
   let acquireSerialPort = mkAcquire
         (Serial.openSerial serialPortPath serialPortSettings)
         Serial.closeSerial
-  serialPortLock <- Lock.new
-  withAcquire acquireSerialPort $ \serialPort -> case cmd of
-    Control controlCmd -> case controlCmd of
-      NoOp -> serialSend serialPort serialPortLock "0"
+  withAcquire acquireSerialPort $ \serialPort -> do
+    serialPortLock <- Lock.new
+    let ctx = Ctx { .. }
+    flip runM ctx $ case cmd of
+      Control controlCmd -> case controlCmd of
+        NoOp -> serialSend serialPort serialPortLock "0"
 
-      Ping -> do
-        res <- serialSendRecv serialPort serialPortLock "1"
-        BS.putStrLn res
+        Ping -> do
+          res <- serialSendRecv serialPort serialPortLock "1"
+          liftIO $ BS.putStrLn res
 
-      Version -> do
-        res <- serialSendRecv serialPort serialPortLock "2"
-        BS.putStrLn res
+        Version -> do
+          res <- serialSendRecv serialPort serialPortLock "2"
+          liftIO $ BS.putStrLn res
 
-      Add i -> do
-        res <- serialSendRecv serialPort serialPortLock $ "3" <> BS.pack
-          (show i)
-        BS.putStrLn res
+        Add i -> do
+          res <- serialSendRecv serialPort serialPortLock $ "3" <> BS.pack
+            (show i)
+          liftIO $ BS.putStrLn res
 
-      Send    -> serialSend serialPort serialPortLock "4"
+        Send    -> serialSend serialPort serialPortLock "4"
 
-      Receive -> do
-        res <- serialSendRecv serialPort serialPortLock "5"
-        BS.putStrLn res
+        Receive -> do
+          res <- serialSendRecv serialPort serialPortLock "5"
+          liftIO $ BS.putStrLn res
 
   putStrLn "Goodbye World!"
