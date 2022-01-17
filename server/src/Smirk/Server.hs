22e722373b5dcca3208cc112334ebd901ed8a801
Brecht Serckx <services+git@brechtserckx.be>
2022-01-17 14:13:25 +0100

    IRKit API

--- server/src/Smirk/Server.hs
+++ server/src/Smirk/Server.hs
@@ -1,24 +1,48 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
 module Smirk.Server
   ( runSmirkServer
   ) where
 
 import           Control.Monad.IO.Class         ( MonadIO(..) )
 import           Data.Acquire                   ( withAcquire )
+import           Data.Aeson
 import           Data.Proxy                     ( Proxy(..) )
+import           GHC.Generics                   ( Generic(..) )
 import qualified Network.Wai.Handler.Warp      as Warp
 import           Servant.API
 import qualified Servant.Server                as Servant
 
 import           Smirk.M
 
-type SmirkApi = EmptyAPI
+data IrSignalType = Raw
+  deriving stock Generic
+  deriving anyclass (FromJSON, ToJSON)
+data IrSignal = IrSignal
+  { format :: IrSignalType
+  , freq   :: Int
+  , data_  :: [Int]
+  }
+  deriving stock Generic
+  deriving anyclass (FromJSON, ToJSON)
+
+-- brittany-disable-next-binding
+type SmirkApi
+  =  "messages"
+  :>  (  Get '[JSON] IrSignal
+    :<|> ReqBody '[JSON] IrSignal
+      :> Post '[JSON] ()
+      )
 
 pSmirkApi :: Proxy SmirkApi
 pSmirkApi = Proxy
 
-smirkServer :: Servant.ServerT SmirkApi m
-smirkServer = Servant.emptyServer
+smirkServer :: Monad m => Servant.ServerT SmirkApi m
+smirkServer = getLatestIrSignal :<|> sendIrSignal
+ where
+  getLatestIrSignal = pure IrSignal { format = Raw, freq = 38, data_ = [] }
+  sendIrSignal _ = pure ()
 
 mToHandler :: MkCtx -> forall a . M a -> Servant.Handler a
 mToHandler (acquireSerialPort, mkCtx) act =
