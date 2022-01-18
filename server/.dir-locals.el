10f7a72ed42c4514bc83420bcd2632d105833356
Brecht Serckx <services+git@brechtserckx.be>
2022-01-18 10:50:24 +0100

    Fix dante

--- server/.dir-locals.el
+++ server/.dir-locals.el
@@ -1,5 +1,10 @@
 ((nil . (
 	 (haskell-mode-stylish-haskell-path . "brittany")
 	 (dante-methods . (new-nix))
+	 ;; (dante-project-root . "/home/brecht/code/smirk")
+	 ;; (dante-repl-command-line .
+   ;;                          ("nix-shell" "shell.nix" "--run"
+   ;;                           ("cabal v2-repl ")
+   ;;                           ))
 	 (dante-target . "smirk-server")
 	 )))
