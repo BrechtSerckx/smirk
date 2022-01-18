cd80f3317618d78ac262d83b7a8e1fc6fa720b3c
Brecht Serckx <services+git@brechtserckx.be>
2022-01-18 10:34:54 +0100

    Use custom api
    We won't be using IRKit anymore

--- server/src/Smirk/Server.hs
+++ server/src/Smirk/Server.hs
@@ -9,8 +9,6 @@ import           Control.Monad.IO.Class         ( MonadIO(..) )
 import           Data.Acquire                   ( withAcquire )
 import           Data.Aeson
 import           Data.Proxy                     ( Proxy(..) )
-import           Data.Text                      ( Text )
-import qualified Data.Text                     as Text
 import qualified Network.Wai.Handler.Warp      as Warp
 import qualified Network.Wai.Middleware.RequestLogger
                                                as Wai
@@ -19,53 +17,33 @@ import qualified Servant.Server                as Servant
 
 import           Smirk.M
 
-data IrSignalType = Raw
-instance FromJSON IrSignalType where
-  parseJSON = withText "IrSignalType" $ \case
-    "raw" -> pure Raw
-    t     -> fail $ "Unknown IrSignalType: " <> Text.unpack t
-instance ToJSON IrSignalType where
-  toJSON = \case
-    Raw -> String "raw"
-data IrSignal = IrSignal
-  { format :: IrSignalType
-  , freq   :: Int
-  , data_  :: [Int]
-  }
+newtype IrSignal = IrSignal {signal  :: Int}
 instance FromJSON IrSignal where
   parseJSON = withObject "IrSignal" $ \o -> do
-    format <- o .: "format"
-    freq   <- o .: "freq"
-    data_  <- o .: "data"
+    signal <- o .: "signal"
     pure IrSignal { .. }
 instance ToJSON IrSignal where
-  toJSON IrSignal {..} =
-    object ["format" .= format, "freq" .= freq, "data" .= data_]
-
-newtype ClientToken = ClientToken {unClientToken :: Text}
-
-instance FromJSON ClientToken where
-  parseJSON = withObject "ClientToken" $ \o -> do
-    unClientToken <- o .: "clienttoken"
-    pure ClientToken { .. }
-instance ToJSON ClientToken where
-  toJSON ClientToken {..} = object ["clienttoken" .= unClientToken]
+  toJSON IrSignal {..} = object ["signal" .= signal]
 
 -- brittany-disable-next-binding
 type SmirkApi
-  =    "messages" :> Get '[JSON] IrSignal
-  :<|> "messages" :> ReqBody '[JSON] IrSignal :> Post '[JSON] ()
-  :<|> "keys" :> Post '[JSON] ClientToken
+  =   "api"
+  :>  "signal"
+  :>  (  -- get last received infrared signal
+         Get '[JSON] IrSignal
+    :<|> -- send infrared signal
+         ReqBody '[JSON] IrSignal
+      :> Post '[JSON] ()
+      )
 
 pSmirkApi :: Proxy SmirkApi
 pSmirkApi = Proxy
 
 smirkServer :: Monad m => Servant.ServerT SmirkApi m
-smirkServer = getLatestIrSignal :<|> sendIrSignal :<|> getClientToken
+smirkServer = getLatestIrSignal :<|> sendIrSignal
  where
-  getLatestIrSignal = pure IrSignal { format = Raw, freq = 38, data_ = [] }
+  getLatestIrSignal = pure IrSignal { signal = 0 }
   sendIrSignal _ = pure ()
-  getClientToken = pure $ ClientToken "foobar"
 
 mToHandler :: MkCtx -> forall a . M a -> Servant.Handler a
 mToHandler (acquireSerialPort, mkCtx) act =
