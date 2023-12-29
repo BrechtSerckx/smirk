4f53a7846d0aa486cd0f1ea97666ddf1ca864874
Brecht Serckx <services+git@brechtserckx.be>
2023-12-29 09:54:40 +0100

    Add haskell.nix

---
+++ default.nix
@@ -0,0 +1,12 @@
+let
+  sources = import ./nix/sources.nix { };
+  haskellNix = import sources.haskellNix { };
+
+  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
+  cleanSource = pkgs.haskell-nix.haskellLib.cleanGit;
+in pkgs.haskell-nix.project {
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "smirk-server";
+    src = ./server;
+  };
+}
--- shell.nix
+++ shell.nix
@@ -1,5 +1,22 @@
-{ pkgs ? import (import ./nix/sources.nix).nixpkgs { } }:
-pkgs.mkShell {
+let
+  project = import ./default.nix;
+  sources = import ./nix/sources.nix { };
+  haskellNix = import sources.haskellNix { };
+  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
+in project.shellFor {
+  packages = ps: [  ];
+
+  withHoogle = true;
+
+  tools = {
+    cabal = "3.6.2.0";
+    hlint = "3.5";
+    ghcid = "0.8.7";
+    ormolu = "0.5.0.1";
+    haskell-language-server = "1.9.1.0";
+    cabal-fmt = "0.1.6";
+  };
+
   buildInputs = with pkgs; [
     # nix
     niv
@@ -11,4 +28,6 @@ pkgs.mkShell {
     # ci
     act
   ];
+
+  exactDeps = true;
 }
