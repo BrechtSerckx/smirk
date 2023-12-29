11bc9c356ba7d039f7d91c879789f21eadc9d4f1
Brecht Serckx <services+git@brechtserckx.be>
2023-12-29 09:08:18 +0100

    Add test for sender mechanism

--- firmware/test/test_gtest/test_RawIRSignal.cpp
+++ firmware/test/test_gtest/test_RawIRSignal.cpp
@@ -1,9 +1,14 @@
 #include <gtest/gtest.h>
 
 #include <cmath>
+#include <list>
+#include <tuple>
+#include <vector>
+
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
+#include "IRSender.h"
 #include "IRSignal.h"
 #include "IRSignal/Raw.h"
 
@@ -19,3 +24,24 @@ TEST(RawIRSignal, decodeJson)
   EXPECT_EQ(hz, 38000);
   EXPECT_EQ(s.buf, std::vector<uint16_t>({9000, 4500}));
  }
+
+class MockIRSender : public IRSender {
+public:
+  std::list<std::tuple<std::vector<uint16_t>, uint16_t>> signalsSent = {};
+  void sendRaw(const std::vector<uint16_t> &buf,
+               const uint16_t hz) {
+    this->signalsSent.push_back(std::make_tuple(buf, hz));
+  };
+};
+TEST(IRSignal, senderMechanism)
+{
+  std::vector<uint16_t> buf = {1, 2};
+  uint16_t hz = 1000;
+  RawIRSignal mockIRSignal = RawIRSignal(buf, hz);
+  MockIRSender irSender = MockIRSender();
+  mockIRSignal.send(&irSender);
+  
+  std::list<std::tuple<std::vector<uint16_t>, uint16_t>> expectedSignalsSent
+    = { std::make_tuple(buf, hz)};
+  EXPECT_EQ(irSender.signalsSent, expectedSignalsSent);
+}
