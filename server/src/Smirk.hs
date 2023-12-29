c2ba0d349b0166754d8699eca30c5b8fc315f034
Brecht Serckx <services+git@brechtserckx.be>
2023-12-29 11:52:48 +0100

    Add request logging

--- server/src/Smirk.hs
+++ server/src/Smirk.hs
@@ -1,6 +1,7 @@
 module Smirk (runSmirk) where
 
 import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai.Middleware.RequestLogger as Wai
 import Smirk.Env (mkEnv)
 import Smirk.Prelude
 import Smirk.Server (app)
@@ -8,4 +9,6 @@ import Smirk.Server (app)
 runSmirk :: IO ()
 runSmirk = do
   env <- mkEnv
-  Warp.run 8080 (app env)
+  putStrLn "Starting server"
+  Warp.run 8080 . (if True then Wai.logStdoutDev else Wai.logStdout) $
+    app env
